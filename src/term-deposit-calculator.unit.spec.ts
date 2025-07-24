208d9543c753b76df45847d289aaa2979c7bb49f
optimistic-updt <kev4tech@gmail.com>
2025-07-24 15:45:28 +1000

    test: wrote maturity tests

---
+++ src/term-deposit-calculator.unit.spec.ts
@@ -0,0 +1,59 @@
+import { calculateTermDeposit } from "./term-deposit-calculator";
+
+const PRINCIPAL = 100000;
+const INTEREST_RATE = 1.1;
+const TERM_IN_MONTHS = 36; // 3 years
+
+describe("[calculateTermDeposit] interest payment occurence", () => {
+  it("when interest is paid MONTHLY", () => {
+    const interestPayment = "monthly";
+
+    const result = calculateTermDeposit(
+      PRINCIPAL,
+      INTEREST_RATE,
+      TERM_IN_MONTHS,
+      interestPayment,
+    );
+
+    expect(result).toBe(103353);
+  });
+
+  it("when interest is paid QUARTERLY", () => {
+    const interestPayment = "quarterly";
+
+    const result = calculateTermDeposit(
+      PRINCIPAL,
+      INTEREST_RATE,
+      TERM_IN_MONTHS,
+      interestPayment,
+    );
+
+    expect(result).toBe(103350);
+  });
+
+  it("when interest is paid at ANNUALLY", () => {
+    const interestPayment = "annually";
+
+    const result = calculateTermDeposit(
+      PRINCIPAL,
+      INTEREST_RATE,
+      TERM_IN_MONTHS,
+      interestPayment,
+    );
+
+    expect(result).toBe(103336);
+  });
+
+  it("when interest is paid at maturity", () => {
+    const interestPayment = "maturity";
+
+    const result = calculateTermDeposit(
+      PRINCIPAL,
+      INTEREST_RATE,
+      TERM_IN_MONTHS,
+      interestPayment,
+    );
+
+    expect(result).toBe(103300);
+  });
+});
